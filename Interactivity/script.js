46f7775caf038d638ed230c3a0ea998bb6cb42f2
Nordream <fedorukoy@gmail.com>
2018-06-15 15:04:34 +0300

    Interaction. Filters

--- Interactivity/script.js
+++ Interactivity/script.js
@@ -1,87 +1,157 @@
-
 (function () {
+	"use strict";
 
-	var w = 350;
-	var h = 400;
+	var h = 300;
+	var w = 700;
+	var padding = 20;
 
-	monthlySales = [
-		{ "month": 10, "sales": 100 },
-		{ "month": 20, "sales": 130 },
-		{ "month": 30, "sales": 250 },
-		{ "month": 40, "sales": 300 },
-		{ "month": 50, "sales": 265 },
-		{ "month": 50, "sales": 310 },
-		{ "month": 60, "sales": 225 },
-		{ "month": 70, "sales": 180 },
-		{ "month": 80, "sales": 120 },
-		{ "month": 90, "sales": 145 },
-		{ "month": 100, "sales": 130 },
-		{ "month": 110, "sales": 190 }
-	];
+	function GetDate(date) {
+		let strDate = new String(date);
 
-	//KPI color
-	function salesKPI(d) {
-		if (d >= 250) { return '#33CC66'; }
-		else if (d < 250) { return '#666666'; }
+		let year = strDate.substr(0, 4);
+		let month = strDate.substr(4, 2) - 1;
+		let day = strDate.substr(6, 2);
+
+		return new Date(year, month, day);
+
 	}
 
-	function showMinMax(ds, col, val, type) {
-		var max = d3.max(ds, function (d) { return d[col]; });
-		var min = d3.min(ds, function (d) { return d[col]; });
+	function buildLine(ds) {
 
-		if (type == 'minmax' && (val == max || val == min)) {
-			return val;
-		}
-		else {
-			if (type == 'all') { return val; }
-		}
+		var svg = d3.select(".container")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h)
+			.attr('id', 'svg-' + ds.category);
+
+		// The data (ds) should be sorted properly
+		let minDate = GetDate(ds.monthlySales[0]['month']);
+		let maxDate = GetDate(ds.monthlySales[(ds.monthlySales.length - 1)]['month']);
+
+		// Scales
+		var xScale = d3.scaleTime()
+			.domain([minDate, maxDate])
+			.range([padding + 5, w - padding]);
+
+		var yScale = d3.scaleLinear()
+			.domain([0, d3.max(ds.monthlySales, (d) => d.sales)])
+			.range([h - padding, 10])
+			.nice();
+
+		// Line
+		var lineFun = d3.line()
+			.x((d) => xScale(GetDate(d.month)))
+			.y((d) => yScale(d.sales));
+
+		// Axis
+		var xAxisGen = d3.axisBottom()
+			.scale(xScale)
+			.tickFormat(d3.timeFormat('%b'));
+
+		var yAxisGen = d3.axisLeft()
+			.scale(yScale);
+		//.ticks(5);
+
+		var xAxis = svg.append('g')
+			.call(xAxisGen)
+			.attr('class', 'x-axis')
+			.attr('transform', 'translate(0, ' + (h - padding) + ')');
+
+		var yAxis = svg.append('g')
+			.call(yAxisGen)
+			.attr('class', 'y-axis')
+			.attr('transform', 'translate(' + padding + ', 0)');
+
+
+		// Drawing the Line
+		var viz = svg.append("path")
+			.attrs({
+				d: lineFun(ds.monthlySales),
+				"stroke": "purple",
+				"stroke-width": 2,
+				"fill": "none",
+				"id": "path-" + ds.category
+			});
 	}
 
-	var svg = d3.select("body")
-		.append("svg")
-		.attr('width', w)
-		.attr('height', h);
+	function updateLine(ds) {
 
-	var dots = svg.selectAll('circles')
-		.data(monthlySales)
-		.enter()
-		.append('circle')
-		.attr('cx', function (d) { return d.month * 3; })
-		.attr('cy', function (d) { return h - d.sales; })
-		.attr('r', 5)
-		.attr('fill', function (d) { return salesKPI(d.sales); });
+		var svg = d3.select(".container").select('#svg-' + ds.category);
 
-	var lables = svg.selectAll('text')
-		.data(monthlySales)
-		.enter()
-		.append('text')
-		.text(function (d) { return showMinMax(monthlySales, 'sales', d.sales, 'all'); })
-		.attr('x', function (d) { return d.month * 3 - 28; })
-		.attr('y', function (d) { return h - d.sales; })
-		.attr('font-size', '12px')
-		.attr('font-family', "sans-serif")
-		.attr("fill", "#666666")
-		.attr("text-anchor", "start");
+		// The data (ds) should be sorted properly
+		let minDate = GetDate(ds.monthlySales[0]['month']);
+		let maxDate = GetDate(ds.monthlySales[(ds.monthlySales.length - 1)]['month']);
 
-d3.select('select')
-.on('change', function(d){
-	let sel=d3.select('#lable-option').node().value;
-	svg.selectAll('text')
-	.data(monthlySales)
-	.text(function(d){
-		return showMinMax(monthlySales, 'sales',d.sales,sel);
-	})
-})
+		// Scales
+		var xScale = d3.scaleTime()
+			.domain([minDate, maxDate])
+			.range([padding + 5, w - padding]);
 
-	var lineFun = d3.line()
-		.x(function (d) { return d.month * 3; })
-		.y(function (d) { return h - d.sales; });
+		var yScale = d3.scaleLinear()
+			.domain([0, d3.max(ds.monthlySales, (d) => d.sales)])
+			.range([h - padding, 10])
+			.nice();
 
-	var viz = svg.append("path")
-		.attr({
-			d: lineFun(monthlySales),
-			"stroke": "purple",
-			"stroke-width": 2,
-			"fill": "none"
+		// Line
+		var lineFun = d3.line()
+			.x((d) => xScale(GetDate(d.month)))
+			.y((d) => yScale(d.sales));
+
+		// Axis
+		var xAxisGen = d3.axisBottom()
+			.scale(xScale)
+			.tickFormat(d3.timeFormat('%b'))
+			.ticks(ds.monthlySales.length - 1);
+
+		var yAxisGen = d3.axisLeft()
+			.scale(yScale);
+		//.ticks(5);
+
+		var xAxis = svg.selectAll('g.x-axis').call(xAxisGen);
+
+		var yAxis = svg.selectAll('g.y-axis').call(yAxisGen);
+
+
+		// Drawing the Line
+		var viz = svg.selectAll("#path-" + ds.category)
+			.attr('d', lineFun(ds.monthlySales));
+	}
+
+	function showHeader(ds) {
+		d3.select(".container").append("h1")
+			.text(ds.category + " Sales (2013)");
+	}
+
+	d3.json(
+		"https://api.github.com/repos/bsullins/d3js-resources/contents/monthlySalesbyCategoryMultiple.json",
+		function (error, data) {
+			if (error) {
+				console.log(error);
+			} else {
+				//console.log(data); //we're golden!
+			}
+			var decodedData = JSON.parse(window.atob(data.content));
+
+			//console.log(decodedData.contents);
+			decodedData.contents.forEach(function (ds) {
+				//console.log(ds);
+				showHeader(ds);
+				buildLine(ds);
+			});
+
+			// Add event listener
+			d3.select('select')
+				.on('change', function (d) {
+					var selection = d3.select('#date-option').node().value;
+					var decodedData = JSON.parse(window.atob(data.content));
+
+					decodedData.contents.forEach(function (ds) {
+						// Filter array according to selection
+						ds.monthlySales.splice(0, ds.monthlySales.length - selection);
+						// Update line
+						updateLine(ds);
+					});
+				});
+
 		});
 })();
